8d4e6fdb04fb8b85432f0dce14eaec93b392693a
Aaron Friedlander <aaron.abf@gmail.com>
2016-09-29 22:36:01 -0400

    Added local vs. production deploy command line arg

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var cloudmine = require('cloudmine');
-var config = require('./config.json');
+
+if (process.argv.indexOf('local') != -1) {
+  var config = require('./config.json');
+} else {
+  var config = {
+    appid: process.env.APPID,
+    apikey: process.env.APIKEY
+  };
+}
 
 var app = express();
 var ws = new cloudmine.WebService({
